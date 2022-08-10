3db1e475e340460165dac9793094a5a4e06b53d0
StarMindz <64619873+StarMindz@users.noreply.github.com>
2022-08-10 08:36:02 +0100

    setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: path.resolve(__dirname, "./src/index.html"),
+    }),
+  ],
+  devServer: {
+    static: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};
